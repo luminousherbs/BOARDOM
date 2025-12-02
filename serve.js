dbeaf84eb9ca71405d5aae61eaf28cc368156d8b
luminousherbs <172189078+luminousherbs@users.noreply.github.com>
2025-12-02 07:32:16 +0000

    Create api for getting posts

--- serve.js
+++ serve.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { promises as fs } from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 
@@ -9,6 +10,16 @@ const port = 26273; // B O A R D
 
 app.use(express.json()); // parse json
 app.use(express.static(path.join(__dirname, "/app/"))); // serve from app
+
+app.get("/api/status", async (req, res) => {
+    res.send(200);
+});
+
+app.get("/api/get-posts", async (req, res) => {
+    const postFile = await fs.readFile("data/posts.json", "utf8");
+    const posts = JSON.parse(postFile);
+    res.send(posts);
+});
 
 app.listen(port, () => {
     console.log(`Listening on ${port}`);
